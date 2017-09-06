4ab30064951faa1bc070dd52ce9ad11a87e6c0d1
Akash Kurian Jose <acash_92@live.com>
2017-09-07 01:34:10 +0530

    added 25 lines of code.

--- Rectangle/watchable.js
+++ Rectangle/watchable.js
@@ -4,6 +4,30 @@
 
 window.Rectangle = window.Rectangle || {}
 
-window.Rectangle.watchable = function watchable(object) {
+window.Rectangle.watchable = function Watchable(object) {
 
+	let reactiveObj = Object.create(Watchable.prototype);
+
+	Object.defineProperty(reactiveObj, '__despatch__', {
+		value: () => {
+
+		}
+	})
+	for (var key in object) {
+		// if(object.hasOwnProperty(key)){
+		let descriptor = {}
+		if(utils.isFunction(object[key])){
+			let boundFn = object[key].bind(reactiveObj);
+			descriptor.value = (...args) => {
+
+				boundFn(...args)
+			}
+		}
+		Object.defineProperty(reactiveObj, key, {
+			set: () => {
+
+			}
+		})
+		// }
+	}
 }
