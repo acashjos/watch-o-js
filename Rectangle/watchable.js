bf7d7dd83b700ac9cdb2ef2b815a680990209d61
Akash Kurian <akashjose@irisind.com>
2017-09-08 20:48:37 +0530

    adaptStructChanges defined. mostly.

--- Rectangle/watchable.js
+++ Rectangle/watchable.js
@@ -1,25 +1,33 @@
 //let utils = require('./utils')
 
-
+// CHILD-UPDATE-HANDLER  is the placeholder for a closure-global function 
+// which receives a nudge whenever a child watchable has changed.
+// This calls despatch thus forwarding the nudge to all parents holding a reference to this.
 
 window.Rectangle = window.Rectangle || {}
 
-window.Rectangle.watchable = function Watchable(object) {
+window.Rectangle.watchable = function Watchable(object, optionalDefaultListener) {
+	// optionalDefaultListener will be added as the first listener - TO.DO
 
 	let reactiveObj;
+	let closureFields;
 
 	let blockDespatch = false;
-	let listeners = new Set();
-	let reference = new Set();
+	let listeners = new Set(); // parents
+	let lastKnownKeySet = new Set(); // properties
+	let watchableChildren = new Map();
+
 	let despatchChanges = function despatchChanges() {
-		adaptStructChanges(reactiveObj, reference);
+		adaptStructChanges(reactiveObj, closureFields);
 		// listeners.forEach
 	}
 
-	let closureFields = {
+	closureFields = {
 		set blockDespatch(val) { blockDespatch = val },
 		get blockDespatch() { return blockDespatch; },
 		listeners,
+		lastKnownKeySet,
+		watchableChildren,
 		despatchChanges,
 	}
 	reactiveObj = generateReactiveStub(closureFields)
@@ -32,14 +40,31 @@ window.Rectangle.watchable = function Watchable(object) {
 	}
 }
 
-function adaptStructChanges(currentObj, lastKnownKeySet) {
-	let temporarySet = new Set();
-	for(let key in currentObj) {
-		if(lastKnownKeySet.has(key)) {
-			temporarySet.add(key) ************ blah blah..
-			2 settum test cheythittu mutually exclusive aayitollathokke address cheyyanam
-		}
+function adaptStructChanges(currentObj, closureFields) {
+
+	let {lastKnownKeys, watchableChildren} = closureFields;
+	let currentKeys = new Set();
+
+	for (let key in currentObj) {
+		currentKeys.add(key);
 	}
+
+	// first detatch removed items
+	lastKnownKeys.forEach(key => {
+		if (currentKeys.has(key)) { // if both set have them, its not a interesting key.
+			currentKeys.delete(key);
+		} else {
+			lastKnownKeys.delete(key);
+			if (watchableChildren.has(key)) {
+				watchableChildren.get(key)._detachListener(CHILD - UPDATE - HANDLER);
+				watchableChildren.delete(key)
+			}
+		}
+	})
+	// attach new properties. everything left in currentKeys is new
+	currentKeys.forEach(key => {
+		animateField(currentObj, key, currentObj[key], closureFields)
+	})
 }
 
 function animateField(parent, fieldName, value, closureFields) {
@@ -56,6 +81,7 @@ function animateField(parent, fieldName, value, closureFields) {
 			return result;
 		}
 	}
+	// else TO.DO
 	Object.defineProperty(parent, fieldName, {
 		set: () => {
 
@@ -70,7 +96,7 @@ function generateReactiveStub(closureFields) {
 
 	Object.defineProperty(reactiveObj, '_attachListener', {
 		value: function _attachListener(listener) {
-			if (utils.isFunction(listener)) 
+			if (utils.isFunction(listener))
 				closureFields.listeners.add(listener);
 		}
 	})
@@ -78,7 +104,7 @@ function generateReactiveStub(closureFields) {
 
 	Object.defineProperty(reactiveObj, '_detachListener', {
 		value: function _detachListener(listener) {
-			if (utils.isFunction(listener)) 
+			if (utils.isFunction(listener))
 				closureFields.listeners.delete(listener);
 		}
 	})
@@ -88,7 +114,7 @@ function generateReactiveStub(closureFields) {
 		value: function _destroy(listener) {
 			// loop through all children Watchables and call detach on them.
 			// This is crusial to avoid trapping references to dead parents.
-			// DO NOT DESTROY if there are listeners attached to this object.
+			// DO NOT DESTROY if there are listeners attached to this object, EXCEPT optionalDefaultListener
 			// Re-attach if any new operation happens on this Object or a new listener attaches.
 			// This should be a temporary unlinked state.
 			// ENSURE THAT ALL REFERENCES (outside of Rectangle) CALL THIS BEFORE CLEARING THEIR REFERENCES
