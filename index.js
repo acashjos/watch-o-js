5c8cee6dc2b1f59b29e756be4f7787728cb169c9
Akash Kurian <akashjose@irisind.com>
2017-09-14 21:45:47 +0530

    Major changes! * source object is no longer modified. creates a new internal clone object for reference * despatches defered using setTimeout(0) * Tests to be rewritten. with defering, despatches become asynchronous. so tests has to be async as well.

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ function WatchO(object, optionalDefaultListener, optionalListOfFieldsToListen) {
 		// attach them on the watchable TO.DO
 		return object;
 	}
-	if (objectMap.has(object)) return objectMap.get(object);
+	if (objectMap.has(object)) {
+		let watchO = objectMap.get(object);
+		watchO._reshape(object);
+		// Object.assign(watchO,object);
+		return watchO
+	}
+
+	if (object && typeof object != "object")
+		throw new TypeError(
+			"Expected an object as first parameter, got "
+			+ (typeof object))
 
 	let reactiveObj;
+	let baseObject = {};
 	let closureFields;
 
 	let blockDespatch = false;
@@ -20,14 +31,19 @@ function WatchO(object, optionalDefaultListener, optionalListOfFieldsToListen) {
 	let watchableChildren = new Map();
 	let isActive = true;
 
+	let pendingDespatch = null;
 	let nudgeWatcher = function nudgeWatcher() {
-		if (blockDespatch) {
-			hasPendingDespatch = true;
+		if (pendingDespatch !== null) {
 			return;
 		}
-		adaptStructChanges(reactiveObj, closureFields);
-		let summary = []; // list of keys changed TO.DO
-		listeners.forEach(listener => listener(summary))
+		
+		pendingDespatch = setTimeout(()=>{
+
+			adaptStructChanges(reactiveObj, reactiveObj, closureFields);
+			let summary = []; // list of keys changed TO.DO
+			listeners.forEach(listener => listener(summary))
+			pendingDespatch=null;
+		}, 0)
 	}
 
 	let onNotify = function onNotify(summary) {
@@ -51,8 +67,18 @@ function WatchO(object, optionalDefaultListener, optionalListOfFieldsToListen) {
 		isActive = true;
 	}
 
+	watchableChildren.$remove = (key) => {
+		watchableChildren.get(key)._detachListener(onNotify)
+		watchableChildren.delete(key);
+	}
+
+	watchableChildren.$add = (key, value) => {
+		value._attachListener(onNotify)
+		watchableChildren.set(key, value);
+	}
+
 	closureFields = {
-		object,
+		object: baseObject,
 		set blockDespatch(val) { blockDespatch = val },
 		get blockDespatch() { return blockDespatch; },
 		// set isActive(val) { isActive = val; },
@@ -68,46 +94,19 @@ function WatchO(object, optionalDefaultListener, optionalListOfFieldsToListen) {
 	}
 
 	reactiveObj = generateReactiveStub(closureFields);
-	let nonEnumerableProps = new Set(Object.getOwnPropertyNames(object));
-
-	function onEach(key) {
-		let propDescriptor = Object.getOwnPropertyDescriptor(object, key)
-		if (propDescriptor.get || propDescriptor.set) {
-			if (propDescriptor.configurable) {
-				propDescriptor.get = propDescriptor.get.bind(reactiveObj);
-				propDescriptor.set = propDescriptor.set.bind(reactiveObj);
-				Object.defineProperty(object, key, propDescriptor)
-			}
-			else throw new TypeError(`Could not modify PropertyDescriptor. \n 
-				PropertyDescriptor for \`baseObject.${key}\` contains Getter and/or Setter and is non configurable too. 
-			\`WatchIt\` has to modify the getter/setter functions to make things work. `)
-		}
-		
-		animateField(reactiveObj, key, object[key], closureFields, propDescriptor.writable)
-		lastKnownKeys.add(key);
-	}
-
-	for (var key in object) {
-		nonEnumerableProps.delete(key);
-		onEach(key);
-	}
-
-	nonEnumerableProps.forEach(onEach)
-	// ((key) => {
-	// 	object.ge
-	// 	onEach(key);
-	// });
 
 	objectMap.set(object, reactiveObj);
+	adaptStructChanges(object, reactiveObj, closureFields);
 	return reactiveObj;
 }
 
-function adaptStructChanges(currentObj, closureFields) {
 
-	let { lastKnownKeys, watchableChildren } = closureFields;
-	let currentKeys = new Set();
+function adaptStructChanges(currentState, reactiveObj, closureFields) {
 
-	for (let key in currentObj) {
+	let { lastKnownKeys, watchableChildren, object: baseObject } = closureFields;
+	let currentKeys = new Set(Object.getOwnPropertyNames(currentState));
+
+	for (let key in currentState) {
 		currentKeys.add(key);
 	}
 
@@ -115,71 +114,104 @@ function adaptStructChanges(currentObj, closureFields) {
 	lastKnownKeys.forEach(key => {
 		if (currentKeys.has(key)) { // if both set have them, its not a interesting key.
 			currentKeys.delete(key);
+
+			// if a watchable exists in the object-tree but not in watchableChildren, add it.
+			if (isWatchable(baseObject[key]) && !watchableChildren.has(key)) {
+				watchableChildren.$add(key, baseObject[key]);
+			}
 		} else {
+			delete reactiveObj[key];
 			lastKnownKeys.delete(key);
 			if (watchableChildren.has(key)) {
-				watchableChildren.get(key)._detachListener(closureFields.onNotify);
-				watchableChildren.delete(key)
+				watchableChildren.$remove(key)
 			}
 		}
+		let propDesc = Object.getOwnPropertyDescriptor(reactiveObj,key);
+		if(propDesc && !propDesc.configurable)
+			throw new TypeError(`Could not modify PropertyDescriptor in currect state at object.${key}`)
 	})
+
 	// attach new properties. everything left in currentKeys is new
 	currentKeys.forEach(key => {
-		animateField(currentObj, key, currentObj[key], closureFields)
+		let propDescriptor = Object.getOwnPropertyDescriptor(currentState, key)
+
+		animateField(reactiveObj, key, propDescriptor, closureFields)
+		lastKnownKeys.add(key);
 	})
 }
 
-function animateField(parent, fieldName, value, closureFields, writable = true) {
 
-	let descriptor = {
+function animateField(reactiveObj, fieldName, propDescriptor, closureFields) {
+
+	propDescriptor.configurable = true;
+	let value = propDescriptor.value;
+
+	// if (Object.hasOwnProperty(reactiveObj, fieldName)) delete reactiveObj[fieldName];
+
+	if (isFunction(value)) {
+		propDescriptor.value = value = setApplyTrap(value, reactiveObj, closureFields)
+	}
+	else if (typeof value == "object") {
+		propDescriptor.value = value = WatchO(value); // Watchable will return same object if the baseobject is already a watchable
+		// value._attachListener(closureFields.onNotify)
+		closureFields.watchableChildren.$add(fieldName, value);
+		// closureFields.object[fieldName] = value
+	}
+	Object.defineProperty(closureFields.object, fieldName, propDescriptor);
+
+
+	let watchoPropDescriptor = {
 		enumerable: true,
 		configurable: true
 	}
 
-	if (Object.hasOwnProperty(parent, fieldName)) delete parent[fieldName]
-	closureFields.object[fieldName] = value;
-
-	if (isFunction(value)) {
-		closureFields.object[fieldName] = setApplyTrap(value, parent, closureFields)
-	}
-	else if (typeof value == "object") {
-		value = WatchO(value); // Watchable will return same object if the baseobject is already a watchable
-		value._attachListener(closureFields.onNotify)
-		closureFields.watchableChildren.set(fieldName, value);
-		closureFields.object[fieldName] = value
-	}
-
-	// else TO.DO
-	if (writable)
-		descriptor.set = (val) => {
-			closureFields.activateIfDead();
-			closureFields.object[fieldName] = val;
-
-			if (isFunction(value)) {
-				closureFields.object[fieldName] = setApplyTrap(value, parent, closureFields)
-			}
-			closureFields.nudgeWatcher();
+	watchoPropDescriptor.set = (val) => {
+		closureFields.activateIfDead();
+		if(!propDescriptor.writable && !propDescriptor.get && !propDescriptor.set) return;
+		let oldVal = closureFields.object[fieldName];
+		let newVal = val;
+		if (isFunction(val)) {
+			if (val == oldVal || val == oldVal.prototype) return;
+			newVal = setApplyTrap(val, reactiveObj, closureFields)
 		}
+		else if (typeof val == "object") {
+			newVal = WatchO(val); // Watchable will return same object if the baseobject is already a watchable
+			if (oldVal == newVal) return;
+			// newVal._attachListener(closureFields.onNotify)
+			closureFields.watchableChildren.set(fieldName, newVal);
+		}
+		if (isWatchable(oldVal)) {
+			// oldVal._detachListener(closureFields.onNotify);
+			closureFields.watchableChildren.$remove(fieldName, oldVal);
+		}
+		if (newVal === oldVal) return;
+		closureFields.object[fieldName] = newVal;
+		closureFields.nudgeWatcher();
+	}
 
-	descriptor.get = () => {
+	watchoPropDescriptor.get = () => {
 		closureFields.activateIfDead();
 		return closureFields.object[fieldName];
 	}
-	Object.defineProperty(parent, fieldName, descriptor)
+	Object.defineProperty(reactiveObj, fieldName, watchoPropDescriptor)
 }
 
 function setApplyTrap(fn, context, closureFields) {
 	// equivalent of `apply` trap
 	let boundFn = fn.bind(context);
-	return (...args) => {
-		if (closureFields.blockDespatch) return boundFn(...args);
-		closureFields.blockDespatch = true;
+
+	let wrappedFn = (...args) => {
+		// if (closureFields.blockDespatch) return boundFn(...args);
+		// closureFields.blockDespatch = true;
 		let result = boundFn(...args);
-		closureFields.blockDespatch = false;
-		if (closureFields.hasPendingDespatch)
+		// closureFields.blockDespatch = false;
+		// if (closureFields.hasPendingDespatch)
 			closureFields.nudgeWatcher();
 		return result;
 	}
+
+	Object.defineProperty(wrappedFn, "prototype", { configurable: false, enumerable: false, value: fn })
+	return wrappedFn
 }
 function isWatchable(object) {
 	return object instanceof WatchO;
@@ -195,26 +227,19 @@ function isFunction(functionToCheck) {
 function generateReactiveStub(closureFields) {
 	closureFields.blockDespatch = false;
 
-	let reactiveObj = Object.create(WatchO.prototype);
+	let inheritedProto = Object.create(WatchO.prototype);
 
-	Object.defineProperty(reactiveObj, '_attachListener', {
+	Object.defineProperty(inheritedProto, '_attachListener', {
 		value: function _attachListener(listener, optionalListOfFieldsToListen) {
 			// `optionalListOfFieldsToListen` if given, call listener only if a field in list is updated. TO.DO
 			if (isFunction(listener)) {
 				closureFields.listeners.add(listener);
-				return closureFields.object;
+				// return closureFields.object;
 			}
 		}
 	})
 
-
-	Object.defineProperty(reactiveObj, '_nudge', {
-		value: function _nudge() {
-			closureFields.nudgeWatcher();
-		}
-	})
-
-	Object.defineProperty(reactiveObj, '_detachListener', {
+	Object.defineProperty(inheritedProto, '_detachListener', {
 		value: function _detachListener(listener) {
 			if (isFunction(listener))
 				closureFields.listeners.delete(listener);
@@ -223,11 +248,30 @@ function generateReactiveStub(closureFields) {
 		}
 	})
 
+	Object.defineProperty(inheritedProto, '_nudge', {
+		value: function _nudge() {
+			closureFields.nudgeWatcher();
+		}
+	})
+
+	Object.defineProperty(inheritedProto, '_reshape', {
+		value: function _reshape(object) {
+			closureFields.destroy();
+			closureFields.watchableChildren.clear();
+			closureFields.lastKnownKeys.clear();
+			Object.getOwnPropertyNames(reactiveObj).forEach(key => delete reactiveObj[key])
+			
+			adaptStructChanges(object, reactiveObj, closureFields);
+			if(objectMap.get(object) != reactiveObj)
+				throw "Warning!! calling _reshape with any object other than the base object is discouraged. However, calling _reshape wrapped in a try-catch will yield the desired result."
+		}
+	})
+
 
 	// Object.defineProperty(reactiveObj, '_destroy', {
 	// 	value: closureFields.destroy
 	// })
-
+	let reactiveObj = Object.create(inheritedProto);
 	return reactiveObj;
 }
 
--- test/watchable.test.js
+++ test/watchable.test.js
@@ -2,250 +2,52 @@ var expect = require('chai').expect;
 
 let WatchO = require('../index');
 
-describe("WatchO()", function () {
-	let baseObj = {
-		foo: 1,
-		invoke: fn => fn(),
-		// get drake() { return "ramoray "+this.foo}
-	}
-
-	let a = WatchO(baseObj)
-
-	it("Should return a pseudo Object that behaves same as the base class.", () => {
-		expect(a).to.be.instanceof(WatchO)
-		expect(a.foo).to.equal(baseObj.foo)
-
-		//more tests needed
-	})
-
-	it("Should should return same WatchO instance when repeatedly called with same baseObject", () => {
-
-		let base = {};
-		let watchable1 = WatchO(base);
-		let watchable2 = WatchO(base);
-
-		expect(watchable1).equals(watchable2);
-	})
-
-	it("Should throw type error if base class " +
-		"has non-configurable propertyDescriptor " +
-		"with getters or setters",
-		() => {
-			let baseObjWithNonConfigurableSetter = {};
-			Object.defineProperty(baseObjWithNonConfigurableSetter, 'foo', {
-				get: () => 55,
-				set: () => { },
-				configurable: false,
-				enumerable: true
-			})
-
-			expect(WatchO.bind(null, baseObjWithNonConfigurableSetter)).to.throw(TypeError);
-
-			baseObjWithNonConfigurableSetter = {}
-			Object.defineProperty(baseObjWithNonConfigurableSetter, 'quaz', {
-				get: () => 55,
-				set: () => { },
-				configurable: false,
-				enumerable: false
-			})
-
-			expect(WatchO.bind(null, baseObjWithNonConfigurableSetter)).to.throw(TypeError);
+describe("WatchO Objects", () => {
+	it("should call listener functions when a value changes", (done) => {
+		let watcho = WatchO({foo:1});
+		watcho._attachListener(function (){
+			watcho._detachListener(arguments.callee);
+			done();
 		})
-});
 
-
-
-describe("WatchO {object}", function () {
-	baseObj = {
-		foo: 1,
-		quak: 2,
-		invoke: fn => fn(),
-		// get drake() { return "ramoray "+this.foo}
-	}
-	let a = WatchO(baseObj)
-
-	it("Should return the base class when an eventListener is registered.", () => {
-		let cb = () => { }
-		z = a._attachListener(cb);
-		expect(z).to.equal(baseObj);
-		a._detachListener(cb);
+		watcho.foo++;
 	})
 
-
-	it("Should update values on set operation and call listeners.", () => {
-		let listenerCallCounter = 0;
-		let cb = () => listenerCallCounter++;
-		a._attachListener(cb)
-		let newValue = 35;
-		a.foo = newValue;
-
-
-		expect(listenerCallCounter, "listner to be called on value changes").to.equal(1);
-		expect(a.foo, "get operation a.foo should return the newly inserted value").to.equal(newValue)
-		expect(a.foo, "get operation basecalss.foo should return newly inserted value").to.equal(newValue)
-
-		a.invoke(() => { a.foo = 3; a.quak = 8 }) //normally this will call listeners twice.
-
-		expect(listenerCallCounter, "invoking member functions should buffer despatches until it returns").greaterThan(1);
-		expect(listenerCallCounter, "invoking member functions should group all despatches until it returns").not.greaterThan(2);
-
-		a._detachListener(cb);
-	})
-
-	it("Can have new properties added from member functions and get listeners called in response .", () => {
-		let listenerCallCounter = 0;
-		let cb = () => listenerCallCounter++;
-		a._attachListener(cb)
-
-		let newValue = { baz: 1, qux: 2 };
-
-		a.car = "newCar";
-		expect(
-			listenerCallCounter,
-			"listner not to be called when new property is added to a watchable object"
-		).to.equal(0);
-
-		a._nudge();
-		expect(listenerCallCounter, "new property in the watchable object should be added during nudge call").to.equal(1)
-
-		a.invoke(() => { a.bar = newValue })
-		expect(
-			listenerCallCounter,
-			"new fields inserted from member functions should trigger listener calls"
-		).to.equal(2)
-		expect(a.bar, "A child object should be converted to WatchO")
-			.to.be.instanceof(WatchO)
-
-		listenerCallCounter = 0;
-		a.bar = { baz: 1, qux: 2 };
-		expect(listenerCallCounter, "listner to be called on value changes on a newly added field").to.equal(1);
-		expect(a.bar, "A child object should be converted to WatchO").to.be.instanceof(WatchO)
-
-		a.bar.baz++;
-		expect(listenerCallCounter, "listner to be called on value changes on a child watchable").to.equal(2);
-
-		a._detachListener(cb);
-	})
-
-
-
-	it("Should handle getters/setters in baseObj", () => {
-
-		baseObj = {
+	it("should be identical to base object", (done) => {
+		let baseObj = {
 			foo: 1,
-			quak: 2,
-			invoke: fn => fn(),
-			get drake() { this.quake = 3; return "ramoray " + this.foo },
-			set drake(value) { this.foo = value; }
+			bar: "string",
+			baz: true,
+			qabb: {
+				dab: 22,
+				melon: "pie"
+			}
 		}
+		let obj1 = WatchO(baseObj);
+		obj1._attachListener(function () {
 
-		let a = WatchO(baseObj)
-
-		let listenerCallCounter = 0;
-		let cb = () => listenerCallCounter++;
-		a._attachListener(cb)
-
-		let temp = a.drake;
-		expect(listenerCallCounter, "modifying a property in a getter in baseObj should call listener").to.equal(1);
-		a.drake = 54;
-		expect(listenerCallCounter, "modifying a property in a setter in baseObj should call listener").to.equal(3);
-		expect(baseObj.foo, "modifying a property in a setter in baseObj should correctly modify properties in baseObj").to.equal(54);
-
-		a._detachListener(cb);
-	})
+			obj1._detachListener(arguments.callee);
+			expect(obj1, "watcho object is not tracking the base object used for creation").not.deep.equals(baseObj);
 
 
-	it("Should handle non-enumerable own properties in baseObj", () => {
+			let obj2 = WatchO(baseObj);
+			expect(obj2, "WatchO called on same object always return same watcho Object").equals(obj1)
+			expect(obj2, "WatchO called on same object return same watcho Object after applying all changes in baseobject").deep.equals(baseObj);
 
-
-		let baseObjWithNonEnumerableProps = {};
-		Object.defineProperty(baseObjWithNonEnumerableProps, 'foo', {
-			value: 10,
-			writable: true,
-			configurable: false,
-			enumerable: false
+			baseObj.tostring = function () {
+				return this.foo + this.bar + this.qabb.dab
+			}
+			obj2 = WatchO(baseObj);
+			expect(obj2.tostring()).equals(baseObj.tostring())
+			done();
 		})
-		Object.defineProperty(baseObjWithNonEnumerableProps, 'bar', {
-			value: 10,
-			writable: false,
-			configurable: false,
-			enumerable: false
-		})
-		Object.defineProperty(baseObjWithNonEnumerableProps, 'baz', {
-			get: () => 55,
-			set: () => { },
-			configurable: true,
-			enumerable: false
-		})
-
-
-		let a = WatchO(baseObjWithNonEnumerableProps)
-
-		let listenerCallCounter = 0;
-		let cb = () => listenerCallCounter++;
-		a._attachListener(cb)
-
-		a.foo = "new";
-		expect(listenerCallCounter,
-			"Should call listener when setting a value on non-enumerable field"
-		).to.equal(1);
-
-
-		a.baz = "new";
-		expect(listenerCallCounter,
-			"Should call listener when setting a value through setter on non-enumerable field"
-		).to.equal(2);
-
-
-		let temp = a.baz;
-		expect(listenerCallCounter,
-			"Should call listener when getting a value through getter on non-enumerable field"
-		).to.equal(2);
-
-
-		a._detachListener(cb);
-	})
-
-
-
-	it("Should handle non-writable properties in base object", () => {
-		let baseObjWithNonWritableProps = {};
-
-		Object.defineProperty(baseObjWithNonWritableProps, 'bar', {
-			value: 10,
-			writable: false,
-			configurable: false,
-			enumerable: false
-		})
-		Object.defineProperty(baseObjWithNonWritableProps, 'car', {
-			value: 10,
-		})
-
-		let a = WatchO(baseObjWithNonWritableProps)
-
-		let listenerCallCounter = 0;
-		let cb = () => listenerCallCounter++;
-		a._attachListener(cb)
-
-		expect(a.bar, "non writable properties should be readable").to.equal(10)
-		a.bar = "new";
-		expect(listenerCallCounter,
-			"Should not call listener when setting a value on non-enumerable non-editable field"
-		).to.equal(0);
-		expect(a.bar, "non writable properties should not change value after set operation").to.equal(10)
-
-
-		expect(a.car, "non writable properties should be readable").to.equal(10)
-		a.car = "new";
-		expect(listenerCallCounter,
-			"Should not call listener when setting a value on non-enumerable non-editable field"
-		).to.equal(0);
-		expect(a.car, "non writable properties should not change value after set operation").to.equal(10)
-
+		expect(obj1).deep.equals(baseObj);
+		obj1.foo = 27;
+		expect(obj1.foo,"get and set should work").equals(27);
+		
+		baseObj.newField = 333;
+		delete baseObj.baz;
+		obj1._nudge()
 
 	})
-
 })
-
-
-// #1  Handle non-enumerable immediate-child properties too.
